761c2d57a2b0542916d8656e0e939f3577010c53
Jhoan Gonzalez <jjgonzalezr17@outlook.com>
2026-07-03 02:15:36 +0000

    terraform labs

---
+++ workspaces/modules/output.tf
@@ -0,0 +1,21 @@
+output "instance_size" {
+  value = aws_instance.server.instance_type
+}
+
+output "instance_id" {
+  value = aws_instance.server.id
+}
+
+output "availability_zone" {
+  value = aws_instance.server.availability_zone
+}
+
+output "instance_name" {
+  value = aws_instance.server.tags.Name
+}
+
+
+# Mejor manera de realizarlo | muestra todo el bloque o recurso
+#output "instance" {
+#  value = aws_instance.server
+#}
